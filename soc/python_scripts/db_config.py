68865064e63615fd9bc12e52a2e0bd2dcd065871
OnlyDeb <maltonlive0@gmail.com>
2025-03-29 17:43:10 +0300

    Принудительная выгрузка кода

--- soc/python_scripts/db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="test1",
-        password="IgNc8Otoa8WBrdrX",
-        database="test1"
-    )
